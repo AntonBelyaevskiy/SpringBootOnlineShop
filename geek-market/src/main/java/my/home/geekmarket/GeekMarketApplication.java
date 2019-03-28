77c09533d21e5899fd707f2474b1469cf4180601
Anton Belyaevskiy <reklamamc@bk.ru>
2019-03-28 18:17:42 +0800

    добавил Классы сущностей, добавил миграцию бд при помощи flyway

--- geek-market/src/main/java/my/home/geekmarket/GeekMarketApplication.java
+++ geek-market/src/main/java/my/home/geekmarket/GeekMarketApplication.java
@@ -1,7 +1,9 @@
 package my.home.geekmarket;
 
+import my.home.geekmarket.configuration.AppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class GeekMarketApplication {
