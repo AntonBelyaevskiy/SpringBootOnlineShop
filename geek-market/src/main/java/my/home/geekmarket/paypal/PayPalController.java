bc19352cf67a37161bc300a894dd9d6b46b080bb
Anton Belyaevskiy <reklamamc@bk.ru>
2019-03-31 13:32:52 +0800

    добавил контроллер PayPal

---
+++ geek-market/src/main/java/my/home/geekmarket/paypal/PayPalController.java
@@ -0,0 +1,106 @@
+package my.home.geekmarket.paypal;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Controller
+@RequestMapping("/paypal")
+public class PayPalController {
+
+    private String clientId = "";
+    private String clientSecret = "";
+    private String mode = "sandbox";
+
+    private APIContext apiContext = new APIContext(clientId, clientSecret, mode);
+
+    @RequestMapping("/buy")
+    public String buy(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Model model
+    ) {
+        try {
+            Payer payer = new Payer();
+            payer.setPaymentMethod("paypal");
+            RedirectUrls redirectUrls = new RedirectUrls();
+            redirectUrls.setCancelUrl("http://localhost:9090/market");
+            redirectUrls.setReturnUrl("http://localhost:9090/market/paypal/success");
+
+            Amount amount = new Amount();
+            amount.setCurrency("RUB");
+            amount.setTotal("1.0");
+
+            Transaction transaction = new Transaction();
+            transaction.setAmount(amount);
+            transaction.setDescription("Покупка в GeekMarket");
+
+            List<Transaction> transactions = new ArrayList<>();
+            transactions.add(transaction);
+
+            Payment payment = new Payment();
+            payment.setPayer(payer);
+            payment.setRedirectUrls(redirectUrls);
+            payment.setTransactions(transactions);
+            payment.setIntent("sale");
+
+            Payment doPayment = payment.create(apiContext);
+
+            Iterator<Links> links = doPayment.getLinks().iterator();
+
+            while (links.hasNext()) {
+                Links link = links.next();
+                if (link.getRel().equalsIgnoreCase("approval_url")) {
+                    return "redirect:" + link.getHref();
+                }
+            }
+        } catch (PayPalRESTException e) {
+            e.printStackTrace();
+        }
+        return "result";
+    }
+
+    @GetMapping("/success")
+    public String success(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Model model
+    ) {
+        try {
+            String paymentId = request.getParameter("paymentId");
+            String payerId = request.getParameter("payerId");
+
+            if (paymentId == null || paymentId.isEmpty() || payerId == null || payerId.isEmpty()) {
+                return "redirect:/";
+            }
+
+            Payment payment = new Payment();
+            payment.setId(payerId);
+
+            PaymentExecution paymentExecution = new PaymentExecution();
+            paymentExecution.setPayerId(payerId);
+
+            Payment executedPayment = payment.execute(apiContext, paymentExecution);
+
+            if (executedPayment.getState().equals("approved")) {
+                model.addAttribute("message", "Ваш заказ сформирован");
+            } else {
+                model.addAttribute("message", "Что-то пошло не так при формировании заказа, попробуйте повторить операцию");
+            }
+
+        } catch (PayPalRESTException e) {
+            e.printStackTrace();
+        }
+        return "page-success";
+    }
+}
